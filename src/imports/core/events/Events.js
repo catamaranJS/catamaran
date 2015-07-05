59d66f87a2e49dd5c439207fe51b80214ef61bbb
Brendon Smith <brendonsmith@seacloud9.org>
2015-07-05 11:27:39 -0700

    adding pub/sub

--- src/imports/core/events/Events.js
+++ src/imports/core/events/Events.js
@@ -1,42 +1,49 @@
 class Events {
-    static on(elementArr, event, fn) {
-        if (typeof elementArr == 'string')
+    
+    constructor() {
+        this.topics = {};
+        this.hOP = this.topics.hasOwnProperty;
+    }
+
+    static on(elementArr = null, event, fn) {
+        if (typeof elementArr == 'string'){
             elementArr = document.querySelectorAll(elementArr);
-        for (var i = 0, len = elementArr.length; i < len; i++) {
-            elementArr[i].addEventListener(event.toLowerCase(), fn)
+            for (var i = 0, len = elementArr.length; i < len; i++) {
+                elementArr[i].addEventListener(event.toLowerCase(), fn)
+            }
+        }else{
+           addEventListener(event.toLowerCase(), fn);
         }
+        
     }
 
-    static off(elementArr, event, fn) {
-        if (typeof elementArr == 'string')
+    static off(elementArr = null, event, fn) {
+        if (typeof elementArr == 'string'){
             elementArr = document.querySelectorAll(elementArr);
-        for (var i = 0, len = elementArr.length; i < len; i++) {
-            elementArr[i].removeEventListener(event.toLowerCase(), fn)
+            for (var i = 0, len = elementArr.length; i < len; i++) {
+                elementArr[i].removeEventListener(event.toLowerCase(), fn)
+            }
+        }else{
+            removeEventListener(event.toLowerCase(), fn);
         }
     }
 
-    static fireEvent(obj, evt) {
-        var fireOnThis = obj;
-        if (document.createEvent) {
-            var evObj = document.createEvent('MouseEvents');
-            evObj.initEvent(evt, true, false);
-            fireOnThis.dispatchEvent(evObj);
-        } else if (document.createEventObject) { //IE
-            var evObj = document.createEventObject();
-            fireOnThis.fireEvent('on' + evt, evObj);
+    subscribe(topic, listener) {
+      var self = this;
+      if(!this.hOP.call(this.topics, topic)) this.topics[topic] = [];
+      var index = this.topics[topic].push(listener) -1;
+      return {
+        remove: function() {
+          delete self.topics[topic][index];
         }
+      }
     }
 
-    static subscribe(obj) {
-        obj.on.apply(obj, arguments);
-    }
-
-    static unsubscribe(obj) {
-        obj.off.apply(obj, arguments);
-    }
-
-    static publish(obj) {
-        obj.fireEvent.apply(obj, arguments);
+    publish(topic, info) {
+      if(!this.hOP.call(this.topics, topic)) return;
+      this.topics[topic].forEach(function(item) {
+            item(info != undefined ? info : {});
+      });
     }
 
 }
